4c7c8975620b500756855013688587d29e2f45a4
ZHANGSHUO22 <zs1822@163.com>
2025-11-18 12:07:06 +0100

    secondChange

---
+++ ft_strnstr.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zshuo <zshuo@student.42prague.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/16 22:18:57 by zshuo             #+#    #+#             */
+/*   Updated: 2025/11/18 12:04:26 by zshuo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strnstr(const char *big, const char *small, size_t len)
+{
+	size_t	i;
+	size_t	j;
+	size_t	small_len;
+
+	if (*small == '\0')
+		return ((char *)big);
+	small_len = ft_strlen(small);
+	if (small_len > len)
+		return (NULL);
+	i = 0;
+	while (big[i] != '\0' && i < len)
+	{
+		if (big[i] == small[0])
+		{
+			j = 0;
+			while (j < small_len && (i + j) < len && big[i + j] == small[j])
+			{
+				j++;
+			}
+			if (j == small_len)
+				return ((char *)&big[i]);
+		}
+		i++;
+	}
+	return (NULL);
+}
